e43f88d2c6476293c1d88b3bc9b13ec8df1ac120
marcloh108 <marc.loh@molisys.com>
2019-11-20 13:44:22 +0700

    Write a program in C to show the first of calender time
    Create function to define and print
    
    Signed-off-by: marcloh108 <marc.loh@molisys.com>

--- question1/main.c
+++ question1/main.c
@@ -5,31 +5,7 @@
 
 int main(void)
 {
-    wchar_t buff[40];
-    struct tm mytime = { .tm_year=116,
-                         .tm_mon=8,
-                         .tm_mday=2,
-                         .tm_hour=17,
-                         .tm_min=51,
-                         .tm_sec=10
-};
-
-    printf("\n The textual representation of specified date and time :\n");
-    if(wcsftime(buff, sizeof buff, L"%A %c", &mytime))
-    {
-        printf("\n%ls\n", buff);
-    }
-    else
-    {
-        puts("wcsftime failed");
-    }
-    setlocale(LC_ALL, "en_US.UTF-8");
-    if (wcsftime(buff, sizeof buff, L"%A %c", &mytime))
-    {
-        printf("%ls\n\n", buff);
-    }
-    else
-    {
-        puts("wcsftime failed");
-    }
+    struct tm first = {.tm_mday=1};
+    mktime(&first);
+    printf("\n%s\n", asctime(&first));
 }
