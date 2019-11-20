2e76173fc8061084fa0eaab121b82eaaa95c5de2
marcloh108 <marc.loh@molisys.com>
2019-11-20 13:22:06 +0700

    Write a program in C to compute the number of seconds passed since the beginning of the month
    Create function to define and print
    
    Signed-off-by: marcloh108 <marc.loh@molisys.com>

--- question1/main.c
+++ question1/main.c
@@ -4,20 +4,17 @@
 
 int main(void)
 {
-    time_t cur_time;
-    char* cur_t_string;
-    cur_time = time(NULL);
-    if (cur_time == ((time_t)-1))
-    {
-        (void) fprintf(stderr, "Failure to get the current time.\n");
-        exit(EXIT_FAILURE);
-    }
-    cur_t_string = ctime(&cur_time); //convert to local time format
-    if (cur_t_string == NULL)
-    {
-        (void) fprintf(stderr, "Failure to convert the current time.\n");
-        exit(EXIT_FAILURE);
-    }
-    (void) printf("\n The Current time is : %s \n", cur_t_string);
-    exit(EXIT_SUCCESS);
+    time_t now;
+    time(&now);
+
+    struct tm beg_month;
+    beg_month = *localtime(&now);
+    beg_month.tm_hour = 0;
+    beg_month.tm_min = 0;
+    beg_month.tm_sec = 0;
+    beg_month.tm_mday = 1;
+
+    double seconds = difftime(now, mktime(&beg_month));
+    printf("\n %.f seconds passed since the beginning of the month.\n\n", seconds);
+    return 0;
 }
